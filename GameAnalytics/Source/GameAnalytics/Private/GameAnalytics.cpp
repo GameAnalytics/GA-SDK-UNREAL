95f3ae00e032b1ffe12a7dd58742d10f09360a30
Martin Treacy-Schwartz <mts@gameanalytics.com>
2019-12-23 10:19:31 +0100

    Updated to v4.0.7

--- GameAnalytics/Source/GameAnalytics/Private/GameAnalytics.cpp
+++ GameAnalytics/Source/GameAnalytics/Private/GameAnalytics.cpp
@@ -14,7 +14,7 @@
 #endif
 #include "Misc/EngineVersion.h"
 
-#define GA_VERSION TEXT("4.0.6")
+#define GA_VERSION TEXT("4.0.7")
 
 UGameAnalytics::UGameAnalytics(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
