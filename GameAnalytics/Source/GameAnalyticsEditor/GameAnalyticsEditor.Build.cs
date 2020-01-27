2d9e5e1b5ef29544253e8d80ecfa337995450c60
Martin Treacy-Schwartz <mts@gameanalytics.com>
2020-01-27 09:52:35 +0100

    Updated to v4.0.12

--- GameAnalytics/Source/GameAnalyticsEditor/GameAnalyticsEditor.Build.cs
+++ GameAnalytics/Source/GameAnalyticsEditor/GameAnalyticsEditor.Build.cs
@@ -32,7 +32,7 @@ namespace UnrealBuildTool.Rules
                     "Http",
                     "Json",
                     "JsonUtilities",
-					"InputCore"
+                    "InputCore"
                 }
             );
 
--- README.md
+++ README.md
@@ -31,6 +31,9 @@ If you have any issues or feedback regarding the SDK, please contact our friendl
 Changelog
 ---------
 <!--(CHANGELOG_TOP)-->
+**4.0.12**
+* uplugin fix
+
 **4.0.11**
 * compile fixes
 
