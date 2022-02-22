9ded7342093514c4cacb59c41f63b57522c144f9
Martin Treacy-Schwartz <mts@gameanalytics.com>
2022-02-22 14:43:51 +0100

    Updated to v5.1.13

--- GameAnalytics/Source/GameAnalytics/Private/GameAnalytics.cpp
+++ GameAnalytics/Source/GameAnalytics/Private/GameAnalytics.cpp
@@ -17,7 +17,7 @@
 #include "Serialization/JsonWriter.h"
 #include "Serialization/JsonSerializer.h"
 
-#define GA_VERSION TEXT("5.1.12")
+#define GA_VERSION TEXT("5.1.13")
 
 UGameAnalytics::UGameAnalytics(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
--- README.md
+++ README.md
@@ -31,6 +31,9 @@ If you have any issues or feedback regarding the SDK, please contact our friendl
 Changelog
 ---------
 <!--(CHANGELOG_TOP)-->
+**5.1.13**
+* fixed bug in internal error reporting
+
 **5.1.12**
 * small corrections
 
